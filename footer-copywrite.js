ecad9bdc79bf197ba434235f9c38441deda684fc
Alberto de Olivira <betodeoliveira@outlook.com>
2023-02-09 17:32:01 -0300

    Check if the year copywrite exists

--- footer-copywrite.js
+++ footer-copywrite.js
@@ -1,9 +1,11 @@
 // when the DOM is ready
-document.addEventListener("DOMContentLoaded", function() { 
-  // get the the span element
-  const yrSpan = document.querySelector('.footer_copywrite-year');
-  // get the current year
-  const currentYr = new Date().getFullYear();
-  // set the year span element's text to the current year
-  yrSpan.textContent = currentYr;
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector('.footer_copywrite-year')) {
+    // get the the span element
+    const yrSpan = document.querySelector('.footer_copywrite-year');
+    // get the current year
+    const currentYr = new Date().getFullYear();
+    // set the year span element's text to the current year
+    yrSpan.textContent = currentYr;
+  }
 });
